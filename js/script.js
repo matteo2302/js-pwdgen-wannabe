fdc55ae1aad98a847880be1350f96d4a4d1b099a
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-05-25 17:08:36 +0200

    js finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log("js.ok");
+//initial
+const passwordPlaceholder = document.getElementById("password");
+console.log(passwordPlaceholder);
+const number = 21;
+console.log(number);
+const nome = prompt("qualè il tuo nome?", "Matteo");
+console.log(nome);
+const cognome = prompt("qualè il tuo cognome?", "Pizzichemi");
+console.log(cognome);
+const color = prompt("qualè il tuo colore preferito?", "Verde");
+console.log(color);
+const password = "" + nome + cognome + color + number;
+console.log(password);
+passwordPlaceholder.innerText = password;
--- steps.md
+++ steps.md
@@ -5,7 +5,7 @@
 - 0 collegare l'id con js
 - 0 stampare la risposta sul browser
 - 0 creare la const +21
-- 1 impostare i 3 pop up con le richieste
-- 2 impostare le const con i dati del pop up
-- 3 raggruppare i dati dei pop up
+- 1 impostare i 3 prompt con le richieste
+- 2 impostare le const con i dati del prompt
+- 3 raggruppare i dati dei prompt
 - 4 aggiungere al collegamento con il DOM i dati
